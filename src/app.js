2345ece53c7619ee2f351612ddc493ee64de6079
Alejandro Patino Camargo <alejandropatinoc@gmail.com>
2024-02-11 21:29:14 -0800

    updated env process

--- src/app.js
+++ src/app.js
@@ -1,28 +1,34 @@
 import { config } from "dotenv";
-import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
-dotenv.config({ path: "./env" });
 import process from "process";
+
 config({ path: "./env" });
+
+const app = express();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(
+    cors({
+      origin: process.env.MP3_API_BASE_URL,
+      credentials: true,
+    }),
+  );
+} else {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+      credentials: true,
+    }),
+  );
+}
+
+app.use(express.json());
 
 import linkRoutes from "./api/routes/linkRoutes.js";
 import getCounter from "./api/routes/counter.js";
-import getLink from "./api/controllers/Links.js";
 
-/*middleware*/
-const app = express();
-app.use(
-  cors({
-    origin: process.env.MP3_API_BASE_URL,
-    credentials: true,
-  }),
-);
-app.use(express.json());
-
-/*routes*/
 app.use("/api/v1", linkRoutes);
 app.use("/api/v1", getCounter);
-console.log(getLink);
 
 export default app;
